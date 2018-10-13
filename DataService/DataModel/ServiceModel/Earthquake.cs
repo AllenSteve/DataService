d832f138eb0efe73898fc529f0a904b96c1b51c6
悲酥清风 <lacrima2012@qq.com>
2018-10-14 06:18:28 +0800

    01.新增DataModel模块；

---
+++ DataService/DataModel/ServiceModel/Earthquake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.ServiceModel
+{
+    public class Earthquake
+    {
+        public string Scale { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string Latitude { get; set; }
+        public string Logitude { get; set; }
+        public string Depth { get; set; }
+        public string Position { get; set; }
+    }
+}
