9c4274393cf1080fe0a0d65d85ec4a75de2363d4
悲酥清风 <lacrima2012@qq.com>
2018-10-14 18:22:14 +0800

    设置启动项目； 更新配置信息；

--- DataService/EarthquakeParser/Program.cs
+++ DataService/EarthquakeParser/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace EarthquakeParser
     {
         static void Main(string[] args)
         {
+            var service = new EarthquakeService();
+            var nodes = service.GetNodes();
+            service.Save(nodes);
         }
     }
 }
