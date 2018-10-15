9b7aaa31b70f88186507d91b566f500054184e8c
悲酥清风 <lacrima2012@qq.com>
2018-10-16 01:05:37 +0800

    添加深圳上市公司数据解析测试；

---
+++ DataService/UnitTest/ServiceTest/StockDataService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Infrastructure;
+
+namespace UnitTest.ServiceTest
+{
+    /// <summary>
+    /// StockDataService 的摘要说明
+    /// </summary>
+    [TestClass]
+    public class StockDataService
+    {
+        public StockDataService()
+        {
+            //
+            //TODO:  在此处添加构造函数逻辑
+            //
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string headStr = "\"data\":";
+            string tailStr = "error";
+            string szUrl = "http://www.szse.cn/api/report/ShowReport/data?SHOWTYPE=JSON&CATALOGID=1110x&TABKEY=tab1&PAGENO=3";
+            string content = WebUtil.Get(szUrl,Encoding.UTF8);
+            content = content.Substring(content.IndexOf(headStr) + headStr.Length, content.IndexOf(tailStr) - tailStr.Length - 2);
+            var json = JsonHelper.DeserializeJsonToObject<Object>(content);
+        }
+    }
+}
