ee7bbe012683f8d0d3fe4c4a5dedae003afd1c8e
悲酥清风 <lacrima2012@qq.com>
2018-11-04 01:26:45 +0800

    迁移实体类；

---
+++ DataService/DataModel/ServiceModel/Stock/StockTradeDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class StockTradeDetail
+    {
+        public int ID { get; set; }
+        public String Exchange { get; set; }
+        public String StockCode { get; set; }
+        public String StockName { get; set; }
+        public String TradeType { get; set; }
+        public Decimal TradePrice { get; set; }
+        public Decimal TradeVolume { get; set; }
+        public Decimal TradeAmount { get; set; }
+        public DateTime TradeDate { get; set; }
+        public DateTime TradeTime { get; set; }
+        public DateTime TradeTimestamp { get; set; }
+    }
+}
